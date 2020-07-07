463795ceebaa8f4ae396ca168f8c7e0f944cd4fc
Kevin Song <celvincolvumn@gmail.com>
2020-07-08 00:46:28 +0900

    mode setting

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -1,1 +1,341 @@
-!function(e){var r={};function o(t){if(r[t])return r[t].exports;var n=r[t]={i:t,l:!1,exports:{}};return e[t].call(n.exports,n,n.exports,o),n.l=!0,n.exports}o.m=e,o.c=r,o.d=function(e,r,t){o.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:t})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(e,r){if(1&r&&(e=o(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var t=Object.create(null);if(o.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var n in e)o.d(t,n,function(r){return e[r]}.bind(null,n));return t},o.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(r,"a",r),r},o.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},o.p="",o(o.s=0)}([function(e,r,o){"use strict";o.r(r);document.querySelector("#root").innerHTML="Hello 1 World 1<br />",document.querySelector("#root").innerHTML+="Hello 2 World 2<br />",document.querySelector("#root").innerHTML+="Hello 3 World 3<br />",document.querySelector("#root").innerHTML+="Hello 4 World 4<br />",document.querySelector("#root").innerHTML+="Hello 5 World 5<br />",document.querySelector("#root").innerHTML+="Hello 6 World 6<br />",document.querySelector("#root").innerHTML+="Hello 7 World 7<br />",document.querySelector("#root").innerHTML+="Hello 8 World 8<br />",document.querySelector("#root").innerHTML+="Hello 9 World 9<br />",document.querySelector("#root").innerHTML+="Hello 10 World 10<br />"}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _src_hello_01__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./src/hello-01 */ \"./src/hello-01.js\");\n/* harmony import */ var _src_hello_02__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./src/hello-02 */ \"./src/hello-02.js\");\n/* harmony import */ var _src_hello_03__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./src/hello-03 */ \"./src/hello-03.js\");\n/* harmony import */ var _src_hello_04__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./src/hello-04 */ \"./src/hello-04.js\");\n/* harmony import */ var _src_hello_05__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./src/hello-05 */ \"./src/hello-05.js\");\n/* harmony import */ var _src_hello_06__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./src/hello-06 */ \"./src/hello-06.js\");\n/* harmony import */ var _src_hello_07__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./src/hello-07 */ \"./src/hello-07.js\");\n/* harmony import */ var _src_hello_08__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./src/hello-08 */ \"./src/hello-08.js\");\n/* harmony import */ var _src_hello_09__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./src/hello-09 */ \"./src/hello-09.js\");\n/* harmony import */ var _src_hello_10__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./src/hello-10 */ \"./src/hello-10.js\");\n/* harmony import */ var _src_world_01__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./src/world-01 */ \"./src/world-01.js\");\n/* harmony import */ var _src_world_02__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./src/world-02 */ \"./src/world-02.js\");\n/* harmony import */ var _src_world_03__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./src/world-03 */ \"./src/world-03.js\");\n/* harmony import */ var _src_world_04__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./src/world-04 */ \"./src/world-04.js\");\n/* harmony import */ var _src_world_05__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ./src/world-05 */ \"./src/world-05.js\");\n/* harmony import */ var _src_world_06__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(/*! ./src/world-06 */ \"./src/world-06.js\");\n/* harmony import */ var _src_world_07__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(/*! ./src/world-07 */ \"./src/world-07.js\");\n/* harmony import */ var _src_world_08__WEBPACK_IMPORTED_MODULE_17__ = __webpack_require__(/*! ./src/world-08 */ \"./src/world-08.js\");\n/* harmony import */ var _src_world_09__WEBPACK_IMPORTED_MODULE_18__ = __webpack_require__(/*! ./src/world-09 */ \"./src/world-09.js\");\n/* harmony import */ var _src_world_10__WEBPACK_IMPORTED_MODULE_19__ = __webpack_require__(/*! ./src/world-10 */ \"./src/world-10.js\");\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\ndocument.querySelector(\"#root\").innerHTML = _src_hello_01__WEBPACK_IMPORTED_MODULE_0__[\"default\"] + \" \" + _src_world_01__WEBPACK_IMPORTED_MODULE_10__[\"default\"] + \"<br />\";\r\ndocument.querySelector(\"#root\").innerHTML += _src_hello_02__WEBPACK_IMPORTED_MODULE_1__[\"default\"] + \" \" + _src_world_02__WEBPACK_IMPORTED_MODULE_11__[\"default\"] + \"<br />\";\r\ndocument.querySelector(\"#root\").innerHTML += _src_hello_03__WEBPACK_IMPORTED_MODULE_2__[\"default\"] + \" \" + _src_world_03__WEBPACK_IMPORTED_MODULE_12__[\"default\"] + \"<br />\";\r\ndocument.querySelector(\"#root\").innerHTML += _src_hello_04__WEBPACK_IMPORTED_MODULE_3__[\"default\"] + \" \" + _src_world_04__WEBPACK_IMPORTED_MODULE_13__[\"default\"] + \"<br />\";\r\ndocument.querySelector(\"#root\").innerHTML += _src_hello_05__WEBPACK_IMPORTED_MODULE_4__[\"default\"] + \" \" + _src_world_05__WEBPACK_IMPORTED_MODULE_14__[\"default\"] + \"<br />\";\r\ndocument.querySelector(\"#root\").innerHTML += _src_hello_06__WEBPACK_IMPORTED_MODULE_5__[\"default\"] + \" \" + _src_world_06__WEBPACK_IMPORTED_MODULE_15__[\"default\"] + \"<br />\";\r\ndocument.querySelector(\"#root\").innerHTML += _src_hello_07__WEBPACK_IMPORTED_MODULE_6__[\"default\"] + \" \" + _src_world_07__WEBPACK_IMPORTED_MODULE_16__[\"default\"] + \"<br />\";\r\ndocument.querySelector(\"#root\").innerHTML += _src_hello_08__WEBPACK_IMPORTED_MODULE_7__[\"default\"] + \" \" + _src_world_08__WEBPACK_IMPORTED_MODULE_17__[\"default\"] + \"<br />\";\r\ndocument.querySelector(\"#root\").innerHTML += _src_hello_09__WEBPACK_IMPORTED_MODULE_8__[\"default\"] + \" \" + _src_world_09__WEBPACK_IMPORTED_MODULE_18__[\"default\"] + \"<br />\";\r\ndocument.querySelector(\"#root\").innerHTML += _src_hello_10__WEBPACK_IMPORTED_MODULE_9__[\"default\"] + \" \" + _src_world_10__WEBPACK_IMPORTED_MODULE_19__[\"default\"] + \"<br />\";\r\n\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ }),
+
+/***/ "./src/hello-01.js":
+/*!*************************!*\
+  !*** ./src/hello-01.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 1\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-01.js?");
+
+/***/ }),
+
+/***/ "./src/hello-02.js":
+/*!*************************!*\
+  !*** ./src/hello-02.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 2\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-02.js?");
+
+/***/ }),
+
+/***/ "./src/hello-03.js":
+/*!*************************!*\
+  !*** ./src/hello-03.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 3\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-03.js?");
+
+/***/ }),
+
+/***/ "./src/hello-04.js":
+/*!*************************!*\
+  !*** ./src/hello-04.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 4\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-04.js?");
+
+/***/ }),
+
+/***/ "./src/hello-05.js":
+/*!*************************!*\
+  !*** ./src/hello-05.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 5\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-05.js?");
+
+/***/ }),
+
+/***/ "./src/hello-06.js":
+/*!*************************!*\
+  !*** ./src/hello-06.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 6\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-06.js?");
+
+/***/ }),
+
+/***/ "./src/hello-07.js":
+/*!*************************!*\
+  !*** ./src/hello-07.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 7\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-07.js?");
+
+/***/ }),
+
+/***/ "./src/hello-08.js":
+/*!*************************!*\
+  !*** ./src/hello-08.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 8\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-08.js?");
+
+/***/ }),
+
+/***/ "./src/hello-09.js":
+/*!*************************!*\
+  !*** ./src/hello-09.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 9\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-09.js?");
+
+/***/ }),
+
+/***/ "./src/hello-10.js":
+/*!*************************!*\
+  !*** ./src/hello-10.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"Hello 10\";\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/hello-10.js?");
+
+/***/ }),
+
+/***/ "./src/world-01.js":
+/*!*************************!*\
+  !*** ./src/world-01.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 1\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-01.js?");
+
+/***/ }),
+
+/***/ "./src/world-02.js":
+/*!*************************!*\
+  !*** ./src/world-02.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 2\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-02.js?");
+
+/***/ }),
+
+/***/ "./src/world-03.js":
+/*!*************************!*\
+  !*** ./src/world-03.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 3\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-03.js?");
+
+/***/ }),
+
+/***/ "./src/world-04.js":
+/*!*************************!*\
+  !*** ./src/world-04.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 4\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-04.js?");
+
+/***/ }),
+
+/***/ "./src/world-05.js":
+/*!*************************!*\
+  !*** ./src/world-05.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 5\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-05.js?");
+
+/***/ }),
+
+/***/ "./src/world-06.js":
+/*!*************************!*\
+  !*** ./src/world-06.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 6\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-06.js?");
+
+/***/ }),
+
+/***/ "./src/world-07.js":
+/*!*************************!*\
+  !*** ./src/world-07.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 7\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-07.js?");
+
+/***/ }),
+
+/***/ "./src/world-08.js":
+/*!*************************!*\
+  !*** ./src/world-08.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 8\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-08.js?");
+
+/***/ }),
+
+/***/ "./src/world-09.js":
+/*!*************************!*\
+  !*** ./src/world-09.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 9\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-09.js?");
+
+/***/ }),
+
+/***/ "./src/world-10.js":
+/*!*************************!*\
+  !*** ./src/world-10.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar word = \"World 10\";\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (word);\r\n\n\n//# sourceURL=webpack:///./src/world-10.js?");
+
+/***/ })
+
+/******/ });
